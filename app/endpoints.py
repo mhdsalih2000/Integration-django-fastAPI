5b72d4efb22c86bad095e5dc00d8ba9df566f9ec
mhd swalih <MuhammedSwalih034@gmail.com>
2024-05-16 14:37:48 +0530

    added endpoints

--- app/endpoints.py
+++ app/endpoints.py
@@ -3,33 +3,63 @@ from fastapi import APIRouter , HTTPException ,status
 from .models import KeyValue
 from .sceemas import KeyValueDTO
 
+
 router = APIRouter()
 
-@router.get("/key-Values", response_model=List[KeyValueDTO])
-async def get_keyValues():
+# endpoint for retrieve data using key - done
+@router.get("/key-values/{key}", response_model=KeyValueDTO)
+def get_key_value(key: str):
     try:
-        key_values = await KeyValue.objects.all()
-        return key_values
+        key_value = KeyValue.objects.get(key=key)
+    except KeyValue.DoesNotExist:
+        raise HTTPException(status_code=404, detail=f"Item with key '{key}' does not exist")
     except Exception as e:
-        return HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e))
+    return key_value
+    
 
 
 
-@router.post("/key-Values")
-async def post_keyValues(key: str, value: str):
-    try:
-        if KeyValue.objects.filter(key=key).exists():
-            raise HTTPException(status_code=400, detail="Key already exists")    
-        KeyValue.objects.create(key=key, value=value) 
-        raise HTTPException(status_code=201, detail="Created")
+
+# endpoint for create new key-value data -done
+@router.post("/key-Values/create")
+def create_keyValues(item: KeyValueDTO):
+    if KeyValue.objects.filter(key=item.key).exists():
+        raise HTTPException(status_code=400 , detail="Key already exists")  
+    try: 
+        KeyValue.objects.create(key=item.key, value=item.value) 
+        return item 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
     
 
-@router.post("/key-Values" ,status_code=status.HTTP_201_CREATED)
-def update():
-    pass
+# endpoint for update value using key -done
+@router.post("/key-Values/update")
+async def update_keyValues(item: KeyValueDTO):
+    try:
+        key_value = KeyValue.objects.filter(key=item.key).first()
+        if key_value:
+            key_value.value = item.value
+            key_value.save()
+            return {"message": "KeyValue updated successfully"}
+        else:
+            raise HTTPException(status_code=404, detail=f"Item with key '{item.key}' does not exist")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+        
 
 
-
+@router.delete("/key-values/delete/{key}")
+def delete_key_value(key: str):
+    try:
+        key_value_data = KeyValue.objects.filter(key=key).first()
+        if key_value_data:
+            key_value_data.delete()
+            return {"message": "KeyValue deleted successfully"}
+        else:
+            raise HTTPException(status_code=404, detail=f"Item with key '{key}' does not exist")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+        
 
--- app/sceemas.py
+++ app/sceemas.py
@@ -3,6 +3,6 @@ from fastapi_utils.api_model import APIModel
 
 
 class KeyValueDTO(APIModel):
-    id :int
+  
     key: str
     value: str
